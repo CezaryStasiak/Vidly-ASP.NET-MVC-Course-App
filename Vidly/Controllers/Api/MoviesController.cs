f18538eeec4d0e6bc8c42b3591d1a7b8024def01
Cezary Stasiak <cezary.p.stasiak@gmail.com>
2018-11-21 10:49:40 +0100

    api/Movies - get movies

--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
+using Vidly.Dtos;
 using Vidly.Models;
 
 namespace Vidly.Controllers.Api
@@ -16,5 +19,12 @@ namespace Vidly.Controllers.Api
         {
             _context = new ApplicationDbContext();
         }
+
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+
     }
 }
